2887f93d92275bb2619448285431dff6875e385e
Eniolasoar <eniolanishe@gmail.com>
2024-06-10 14:52:40 +0100

    navbar responsive update

--- src/PageComponents/NavBar.jsx
+++ src/PageComponents/NavBar.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 function NavBar() {
     return (
         
-            <div className='flex justify-between items-center w-[92%] mx-auto py-4'>
+            <div className='flex justify-between items-center w-[80%] mx-auto py-4'>
                 <div className=''><img src="/images/logo.png" alt="" /></div>
                 <div className='flex gap-4 items-center justify-center '>
-                    <ul className='flex list-none gap-4'>
+                    <ul className=' list-none gap-10 items-center hidden sm:flex '>
                         <li className='text-black100 font-poppins font-medium hover:text-lightBlue hover:cursor-pointer'>Features</li>
                         <li  className='text-black100 font-poppins font-medium hover:text-lightBlue hover:cursor-pointer' >About Us</li>
                         <li className='text-black100 font-poppins font-medium hover:text-lightBlue hover:cursor-pointer'>Blog</li>
-                        
+                        <li className='text-black100 font-poppins font-medium bg-lightGray p-2 rounded-md hover:bg-lightBlue hover:cursor-pointer' >
+                        Log in
+                    </li>
                     </ul>
 
-                    <div className='text-black100 font-poppins font-medium bg-lightGray p-2 rounded-md hover:bg-lightBlue hover:cursor-pointer' >
-                        Log in
-                    </div>
+                    
 
-                    <div>
-                        <img src="/images/menu.png" alt="" className='md:hidden' />
+                    <div className='block sm:hidden'>
+                        <ion-icon name="menu-outline" ></ion-icon>
                     </div>
 
                 </div>
